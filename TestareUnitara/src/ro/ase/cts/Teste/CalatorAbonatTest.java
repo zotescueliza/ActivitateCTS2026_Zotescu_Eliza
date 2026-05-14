4adbff1cb0b74fa8b4787ff3926d40ab922413f6
Zotescu Eliza <zotescumaria23@stud.ase.ro>
2026-05-14 18:56:10 +0300

    Testare Unitara

--- TestareUnitara/src/ro/ase/cts/Teste/CalatorAbonatTest.java
+++ TestareUnitara/src/ro/ase/cts/Teste/CalatorAbonatTest.java
@@ -1,4 +1,25 @@
-import static org.junit.Assert.*;
+package ro.ase.cts.Teste;
+
+import org.junit.Assert;
+import ro.ase.cts.clase.CalatorAbonat;
+
 public class CalatorAbonatTest {
-  
+
+    @org.junit.Test
+    public void testplatesteBilet() {
+        CalatorAbonat calator = new CalatorAbonat("Nick");
+        calator.setSold(5.5f);
+        calator.platesteBilet(1f);
+        Assert.assertEquals(calator.getSold(),4.5f,0.001f);
+    }
+
+    @org.junit.Test
+    public void testPlatesteBiletInsuficient(){
+        CalatorAbonat calator = new CalatorAbonat("Irina");
+        calator.setSold(5.5f);
+        calator.platesteBilet(10f);
+        Assert.assertEquals(5.5f,calator.getSold(),0.001f);
+
+    }
+
 }
--- TestareUnitara/src/ro/ase/cts/clase/CalatorAbonat.java
+++ TestareUnitara/src/ro/ase/cts/clase/CalatorAbonat.java
@@ -1,4 +1,41 @@
 package ro.ase.cts.clase;
 
-public class CalatorAbonat {
+public class CalatorAbonat{
+
+    String nume;
+    private float sold;
+
+    public void platesteBilet(float pret){
+        if(sold > pret){
+            System.out.println(this.nume + " plateste " + pret + " pentru o calatorie");
+            sold -= pret;
+        }
+        else{
+            System.out.println("SOLD INSUFICIENT! Mergeti va rog pe jos");
+        }
+    }
+
+    public CalatorAbonat(String nume) {
+        this.nume = nume;
+    }
+
+    public void notificare(String nrLinie) {
+        System.out.println( "Calator " + this.nume + " " + nrLinie);
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public float getSold() {
+        return sold;
+    }
+
+    public void setSold(float sold) {
+        this.sold = sold;
+    }
 }
